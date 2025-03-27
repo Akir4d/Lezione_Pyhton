e24602f8f461cfbcaf3e7394b2d64a49354c607e
Paolo Rampino <liadserv@gmail.com>
2025-03-27 11:26:21 -0400

    first commit

---
+++ base.py
@@ -0,0 +1,11 @@
+a=30
+print(a)
+a="Prova"
+print(a)
+a=b"Prova"
+print(a)
+a=30
+b=70
+print(a,"+",b,"=",a+b)
+print(f"la somma di {a} e {b} e' uguale a {a+b}")
+print(str(a) + " + " + str(b) + " = ",a+b)
---
+++ stringhe.py
@@ -0,0 +1,14 @@
+x="questa e' una stringa"
+print(x.capitalize())
+print(x.split())
+print(x.upper()) # ciao
+'''
+Questo pero' e' 
+un falso commento
+'''
+"Questo e' un commento falso"
+# Questo e' un vero commento
+print('''
+Questo pero' e' 
+un falso commento
+''')
